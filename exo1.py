080cb0a49b2707e0e96b2819167ace7633f7ac32
Flofrost <ballagerflorian@gmail.com>
2023-01-05 16:53:01 +0100

    expanded ex1, there's a spooky in the code

--- exo1.py
+++ exo1.py
@@ -2,9 +2,9 @@ from time import sleep
 
 class Robot():
 
-    powerState = 0
+    __powerState = 0
     __batteryLevel = 0
-    speed = 0
+    __speed = 0
 
     def __init__(self, name="romeo"):
         self.name = name
@@ -12,20 +12,52 @@ class Robot():
     def __str__(self):
         return "\
             \rPower State : {power}\n\
-            \rBattery Level : {battery}\n\
-            \rCurrent Velocity : {speed}\n\
+            \rBattery Level : {battery}%\n\
+            \rCurrent Velocity : {speed} m/s\n\
             \r".format(\
-                power="none",\
-                battery=str(self.__batteryLevel)+"%",\
-                speed="none")
+                power="On" if self.__powerState else "Off",\
+                battery=self.__batteryLevel,\
+                speed=self.__speed)
             
     def chargeBattery(self):
         while(self.__batteryLevel < 100):
             self.__batteryLevel += 1
+            print("\rCharging... {}%   ".format(self.__batteryLevel),end="")
             sleep(0.1)
+        print("Charging Complete!")
+        
+    def powerOn(self):
+        if self.__batteryLevel:
+            self.__powerState = 1
+    
+    def powerOff(self):
+        self.stop()
+        self.__powerState = 0
+        
+    def stop(self):
+        self.__speed = 0
+
+    @property
+    def speed(self):
+        return self.__speed
+    
+    @speed.setter
+    def speed(self,speed):
+        if self.__powerState:
+            self.__batteryLevel -= 5
+            self.__speed = min(speed,300000000)
+            
+    @batteryLevel.setter
+    def batteryLevel(self,batteryLevel):
+        if batteryLevel < 0:
+            self.__batteryLevel = 0
+            self.powerOff()
+        else:
+            self.__batteryLevel = batteryLevel
 
 r = Robot("bob")
 
+r.speed = 65486
 print(r)
 r.chargeBattery()
 print(r)
