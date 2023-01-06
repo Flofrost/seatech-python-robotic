37251806923dee1459295de2b4a15afdb81b56f1
Flofrost <ballagerflorian@gmail.com>
2023-01-06 09:32:56 +0100

    continued ex2

--- exo2.py
+++ exo2.py
@@ -17,23 +17,23 @@ class Hooman:
                      belly = self.__belly)
         
     def digest(self):
-        pass
+        self.__belly -= 42
+        self.__belly = max(0,self.__belly)
 
     def eat(self, foods):
-        if isinstance(foods, Food):
-            try:
-                iter(foods)
-            except TypeError:
-                if self.__belly < 1000:
-                    self.__belly += foods.mass
-            else:
-                for food in foods:
-                    if self.__belly < 1000:
-                        self.__belly += food.mass
+        try:
+            iter(foods)
+        except TypeError:
+            if self.__belly < self.__maxBelly and isinstance(foods, Food):
+                self.__belly += foods.mass
+        else:
+            for food in foods:
+                if self.__belly < self.__maxBelly and isinstance(food, Food):
+                    self.__belly += food.mass
+        self.__belly = min(self.__belly, self.__maxBelly)
                     
-        
-        def exist(self):
-            self.digest()
+    def exist(self):
+        self.digest()
         
 
 class Food:
@@ -43,9 +43,24 @@ class Food:
     def __init__(self, mass):
         self.mass = mass
             
+            
+class Cyborg(exo1.Robot,Hooman):
+    
+    def __init__(self, name="romeo"):
+        super().__init__(name)
+
+    def __str__(self):
+        return super().__str__()
+
+    def fun(self):
+        pass
+
 
 h = Hooman(0)
 apple = Food(170)
+tiramisu = Food(343)
 print(h)
-h.eat(apple)
+h.eat([apple,tiramisu])
+print(h)
+h.exist()
 print(h)
