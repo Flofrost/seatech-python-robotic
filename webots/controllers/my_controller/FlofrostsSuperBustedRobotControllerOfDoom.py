04206987a11830ea61912fc96963265c45441ea9
Flofrost <ballagerflorian@gmail.com>
2023-01-20 11:49:55 +0100

    added gps and accel support

--- webots/controllers/my_controller/FlofrostsSuperBustedRobotControllerOfDoom.py
+++ webots/controllers/my_controller/FlofrostsSuperBustedRobotControllerOfDoom.py
@@ -1,5 +1,5 @@
 import Fmath
-from controller import Robot,Motor
+from controller import Robot,Motor,GPS,Accelerometer
 
 class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSUperSmashBrosGame(Robot):
     
@@ -10,12 +10,16 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSUperSmashBrosGame(
 
         self.__pitchMotor = Motor("body pitch motor")
         self.__yawMotor = Motor("body yaw motor")
-        self.__yawSensor = self.__yawMotor.position_sensor()
-
         self.__pitchMotor.setPosition(float("inf"))
         self.__yawMotor.setPosition(float("inf"))
+
+        self.__gps = GPS("gps")
+        self.__gps.enable(100)
+        
+        self.__accelerometer = Accelerometer("body accelerometer")
+        self.__accelerometer.enable(100)
 
     def proceed(self):
         self.__pitchMotor.setVelocity(self.__velocipy.modulus)
         self.__yawMotor.setVelocity(self.__velocipy.angle)
-        print(f"Speed : {self.__pitchMotor.getVelocity()}, Angle : {self.__yawSensor.getValue()}")
+        print(f"GPS : {self.__gps.value}, Accel : {self.__accelerometer.value}")
