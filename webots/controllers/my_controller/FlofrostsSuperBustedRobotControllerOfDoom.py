0a6c0f83ad8bac9cd11b52d9e1d0553fef90b52e
Flofrost <ballagerflorian@gmail.com>
2023-01-27 14:22:36 +0100

    PID system "works", bobot goes to position 0!

--- webots/controllers/my_controller/FlofrostsSuperBustedRobotControllerOfDoom.py
+++ webots/controllers/my_controller/FlofrostsSuperBustedRobotControllerOfDoom.py
@@ -1,6 +1,6 @@
-from tabnanny import verbose
 import Fmath
-from math import pi, cos
+from math import pi
+from random import random
 from controller import Robot,Motor,GPS,InertialUnit
 
 arenaLim1 = [-4, -4]
@@ -11,7 +11,7 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
     __targetDir = Fmath.ComplexN(3,0)
     __targetPos = Fmath.ComplexN(0,0)
     
-    __angleError = 0
+    __angleErrorPID = [0, 0, 0]
     
     __verboseMode = "log"
 
@@ -31,7 +31,7 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
 
         if self.__verboseMode == "log":
             self.__logFile = open("log.csv", "w")
-            self.__logFile.write("Pos X, Pos Z, rotate, dirAngle, err\n")
+            self.__logFile.write("Pos X, Pos Z, rotate, dirAngle, P, I\n")
             
     def __del__(self):
         if self.__verboseMode == "log":
@@ -43,22 +43,36 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
                      {self.__gps.value[1]:.03f},\
                      {self.angle:.03f},\
                      {self.__targetDir.angle:.03f},\
-                     {self.__angleError:.03f}\n".replace(" ","")
+                     {self.__angleErrorPID[0]:.03f},\
+                     {self.__angleErrorPID[1]:.03f}\n".replace(" ","")
 
     def goTo(self,newTargetPosition:Fmath.ComplexN):
         self.__targetPos = newTargetPosition
 
     def applySpeedVector(self, vector:Fmath.ComplexN):
-        self.__Lmotor.setVelocity(vector.modulus * (cos(vector.angle) if vector.angle > 0 else 1))
-        self.__Rmotor.setVelocity(vector.modulus * (cos(vector.angle) if vector.angle < 0 else 1))
+        offset = pi/4
+        slope = 3.74
+        self.__Lmotor.setVelocity(min(16, max(-16, vector.modulus * (Fmath.sigmoid(vector.angle, slope=-slope, offset=-offset) * 2 - 1))))
+        self.__Rmotor.setVelocity(min(16, max(-16, vector.modulus * (Fmath.sigmoid(vector.angle, slope= slope, offset= offset) * 2 - 1))))
 
     def proceed(self):
-        self.__targetDir = self.position - self.__targetPos
-        self.__angleError += (self.__targetDir.angle - self.angle) * 0.01
+        self.__targetDir = self.__targetPos - self.position
+        angleError = self.__targetDir.angle - self.angle
+        self.__angleErrorPID[0] =  angleError * 1
+        self.__angleErrorPID[1] += angleError * (1 / 50)
         
-        self.applySpeedVector(Fmath.ComplexN(10, max(min(self.__angleError, pi/2), -pi/2) * -1, mode=1))
+        correctionVelocity = Fmath.ComplexN(self.__targetDir.modulus * 10,
+                                            self.__angleErrorPID[0] + self.__angleErrorPID[1],
+                                            mode=1)
+        correctionVelocity.modulus *= max(0, correctionVelocity.dotProduct(self.__targetDir))
+        self.applySpeedVector(correctionVelocity)
 
-        print(self, self.__Lmotor.getVelocity(), self.__Rmotor.getVelocity())
+        # print(self, self.__Lmotor.getVelocity(), self.__Rmotor.getVelocity())
+
+        if self.__targetDir.modulus < 0.3:
+            self.__targetPos.real = random() * 8 - 4
+            self.__targetPos.imaginary = random() * 8 - 4
+            print(f"Changing target to position {self.__targetPos} !!")
 
         if self.__verboseMode == "log":
             self.__logFile.write(str(self))       
