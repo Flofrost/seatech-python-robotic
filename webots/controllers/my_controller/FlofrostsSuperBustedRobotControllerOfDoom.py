371020106afad0413e7887fee22cc1f2e5c4e692
Flofrost <ballagerflorian@gmail.com>
2023-01-30 11:27:04 +0100

    sad

--- webots/controllers/my_controller/FlofrostsSuperBustedRobotControllerOfDoom.py
+++ webots/controllers/my_controller/FlofrostsSuperBustedRobotControllerOfDoom.py
@@ -11,19 +11,26 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
     __targetDir = Fmath.ComplexN(3,0)
     __targetPos = Fmath.ComplexN(0,0)
     
-    __angleErrorPID = [0, 0, 0]
+    __pid = [0, 0, 0]
+    __prev = 0
     
     __verboseMode = "log"
-    __robot = "roomba"
+    __robot = "bb8"
 
     def __init__(self):
         super().__init__()
 
-
-        self.__Lmotor = Motor("left wheel motor")
-        self.__Lmotor.setPosition(float("inf"))
-        self.__Rmotor = Motor("right wheel motor")
-        self.__Rmotor.setPosition(float("inf"))
+        if self.__robot == "roomba":
+            self.__Lmotor = Motor("left wheel motor")
+            self.__Lmotor.setPosition(float("inf"))
+            self.__Rmotor = Motor("right wheel motor")
+            self.__Rmotor.setPosition(float("inf"))
+        elif self.__robot == "bb8":
+            self.__Lmotor = Motor("body pitch motor")
+            self.__Lmotor.setPosition(float("inf"))
+            self.__Rmotor = Motor("body yaw motor")
+            self.__Rmotor.setPosition(float("inf"))
+            self.__Rmotor.setVelocity(0)
 
         self.__gps = GPS("gps")
         self.__gps.enable(int(self.getBasicTimeStep()))
@@ -33,7 +40,7 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
 
         if self.__verboseMode == "log":
             self.__logFile = open("log.csv", "w")
-            self.__logFile.write("Wel L, Wel R, rotate, dirAngle, P, I\n")
+            self.__logFile.write("Wel L, Wel R, rotate, dirAngle, Err P, Err I\n")
             
     def __del__(self):
         if self.__verboseMode == "log":
@@ -41,12 +48,12 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
 
     def __str__(self) -> str:
         if self.__verboseMode == "log":
-            return f"{self.__Lmotor.getVelocity() / 16:.03f},\
-                     {self.__Rmotor.getVelocity() / 16:.03f},\
+            return f"{self.__Lmotor.getVelocity():.03f},\
+                     {self.__Rmotor.getVelocity():.03f},\
                      {self.angle:.03f},\
                      {self.__targetDir.angle:.03f},\
-                     {self.__angleErrorPID[0]:.03f},\
-                     {self.__angleErrorPID[1]:.03f}\n".replace(" ","")
+                     {self.__pid[0]:.03f},\
+                     {self.__pid[1]:.03f}\n".replace(" ","")
 
     def goTo(self,newTargetPosition:Fmath.ComplexN):
         self.__targetPos = newTargetPosition
@@ -59,13 +66,19 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
 
     def proceed(self):
         self.__targetDir = self.__targetPos - self.position
-        angleError = self.__targetDir.angle - self.angle
+        self.__angleError = self.__targetDir.angle - self.angle
+        self.__pid[0] = self.__angleError * -1.5
+        self.__pid[1] += self.__angleError * 0
         
-        correctionVelocity = Fmath.ComplexN(self.__targetDir.modulus * 10,
-                                            -angleError,
-                                            mode=1)
-        correctionVelocity.modulus *= max(0, correctionVelocity.dotProduct(self.__targetDir))
-        self.applySpeedVector(correctionVelocity)
+        if self.__robot == "roomba":
+            correctionVelocity = Fmath.ComplexN(-self.__targetDir.modulus * 1,
+                                                10 if self.__angleError < 0 else -10,
+                                                mode=1)
+            correctionVelocity.modulus *= max(0, correctionVelocity.dotProduct(self.__targetDir))
+            self.applySpeedVector(correctionVelocity)
+        elif self.__robot == "bb8":
+            self.__Lmotor.setVelocity(self.__targetDir.modulus * 2)
+            self.__Rmotor.setVelocity(self.__targetDir.angle * 3)
 
         # print(self, self.__Lmotor.getVelocity(), self.__Rmotor.getVelocity())
 
@@ -76,6 +89,8 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
 
         if self.__verboseMode == "log":
             self.__logFile.write(str(self))       
+
+        self.__prev = self.__angleError
         
     @property
     def position(self) -> Fmath.ComplexN:
@@ -83,7 +98,10 @@ class FlofrostsSuperDuperRobotOfEpicnessThatWillTotallyWinTheSuperSmashBrosGame(
     
     @property
     def angle(self) -> float:
-        return self.__rotate.roll_pitch_yaw[2]
+        if self.__robot == "roomba":
+            return self.__rotate.roll_pitch_yaw[2]
+        elif self.__robot == "bb8":
+            return self.__rotate.roll_pitch_yaw[2]
     
     @property
     def velocity(self) -> Fmath.ComplexN:
